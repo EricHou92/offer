f2c23ea6e7edd1bb0127f18f8778b419d44070bd
ciciyaya <1551723566@qq.com>
2019-07-25 16:22:36 +0800

    Update N18_EqualSubSum.java

--- src/A2_Leetcode/T3_DynamicProgramming/package1/N18_EqualSubSum.java
+++ src/A2_Leetcode/T3_DynamicProgramming/package1/N18_EqualSubSum.java
@@ -19,15 +19,8 @@ public class N18_EqualSubSum {
         sum /= 2;
         int n = nums.length;
         boolean[][] dp = new boolean[n+1][sum+1];
-        for (int i = 0; i < dp.length; i++) {
-            Arrays.fill(dp[i], false);
-        }
-        dp[0][0] = true;
-        for (int i = 1; i < n+1; i++) {
+        for (int i = 0; i < n+1; i++) {
             dp[i][0] = true;
-        }
-        for (int j = 1; j < sum+1; j++) {
-            dp[0][j] = false;
         }
         for (int i = 1; i < n+1; i++) {
             for (int j = 1; j < sum+1; j++) {
