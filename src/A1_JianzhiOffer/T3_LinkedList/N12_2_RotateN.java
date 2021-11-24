e528aa368b1cd3d111443f18fe379330e429e777
houerhang <houerhang@bigo.sg>
2021-11-24 16:19:42 +0800

    fix

--- src/A1_JianzhiOffer/T3_LinkedList/N12_2_RotateN.java
+++ src/A1_JianzhiOffer/T3_LinkedList/N12_2_RotateN.java
@@ -7,18 +7,18 @@ public class N12_2_RotateN {
 
     //给定列表，将列表向右旋转k个位置
     //Given 1->2->3->4->5->NULL and k = 2,  return 4->5->1->2->3->NULL.
-    public ListNode rotateRight(ListNode head, int n) {
+    public ListNode rotateRight(ListNode head, int k) {
         if (head==null || head.next==null)
             return head;
         ListNode fast = head;
         ListNode slow = head;
-        int len = 0;
-        while (head != null) {
-            head = head.next;
+        int len = 1;
+        while (fast.next != null) {
+            fast = fast.next;
             len++;
         }
-        for (int j=len-n%len; j>1; j--) //Get the len-n%len th node
-            slow = slow.next;
+        for(int i=1; i < len-k%len; i++)
+            slow = slow.next;   //找到链表的第len-k个节点
         fast.next = head;
         head = slow.next;
         slow.next = null;
